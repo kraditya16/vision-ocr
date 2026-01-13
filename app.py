eaf22393db8641aa1b1d1ee20a2e25bcf64bf3b8
Aditya <kraditya162003@gmail.com>
2026-01-13 16:41:34 +0530

    hi

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def analyze():
     return render_template('result.html', text="\n".join(text_output))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
